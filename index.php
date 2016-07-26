16893e5d88946561b2d85398e557600bf98bd79e
IFA11BT201 <nikogs95+schule@gmail.com>
2016-07-26 08:06:08 +0200

    commit

--- index.php
+++ index.php
@@ -13,41 +13,7 @@ $itverwaltung = new itverwaltung();
 <body>
     <div class="Container">
         <div class="row">
-            <nav class="navbar navbar-default" id="navbar">
-                <div class="container-fluid">
-                    <div class="navbar-header"> <a class="navbar-brand" href="#">
-                        IT-Verwaltung
-                    </a> </div>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="navbar-text">Willkommen: Benutzer</li>
-                        <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <!-- Dropdown Navigation-->
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <!-- Column width 3-->
-            <div class="col-md-3">
-                <!-- Vertikale Navigation-->
-                <ul class="nav nav-pills nav-stacked" id="verNav">
-                    <li class="active"><a href="index.php">Startseite</a></li>
-                    <li><a href="Stammdatenverwaltung.php">Stammdatenverwaltung</a></li>
-                    <li><a href="#">Neuanschaffungen</a></li>
-                    <li><a href="#">Wartungsarbeiten</a></li>
-                    <li><a href="#">IT-Ausstattung</a></li>
-                    <li><a href="#">Abfragen</a></li>
-                </ul>
-            </div>
-            <!--/ Column width 3-->
+            
             <!-- Column width 9-->
             <div class="col-md-9" id="mainDiv"> Test<?php $itverwaltung->getAllRooms();?> </div>
             <!--/ Column width 9-->
--- menu.php
+++ menu.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Copyright (C) 2016 Administrator
  *
@@ -22,7 +21,10 @@
  * @author Niko GS
  * @
  */
+
+
 $menu = [
+    'header' => 'IT-Verwaltung',
     'typ' => 'Name',
     'dropdown' => array(
         "menutext" => "Dropdown",
@@ -31,6 +33,74 @@ $menu = [
         "link" => "link",
     ),
 ];
-function printmenu($menu, $Type){
-    
+
+function printmenu($menu, $type) {
+    ?>
+    <nav class="navbar navbar-default" id="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header"> <a class="navbar-brand" href="#">
+                    <?= $menu['header'] ?>
+                </a> </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="navbar-text">Willkommen: <?= $user ?></li>
+
+                <?php
+                if (isset($menu['dropdown'])) {
+                    foreach ($menu['dropdown'] as $key => $value) {
+                        ?>
+                        <li class = "dropdown">
+                            <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown" role = "button" aria-haspopup = "true" aria-expanded = "false">
+                                <?php
+                                if ($key == "menutext") {
+                                    echo $value;
+                                }
+                                ?><span class="caret"></span>
+                            </a>
+                            <!-- Dropdown Navigation-->
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                        <?php
+                    }
+                }
+                ?>
+                <li class="dropdown"> 
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+    <?php if ($key == "")  ?><span class="caret"></span>
+                    </a>
+                    <!-- Dropdown Navigation-->
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Column width 3-->
+    <div class="col-md-3">
+        <!-- Vertikale Navigation-->
+        <ul class="nav nav-pills nav-stacked" id="verNav">
+            <li class="active"><a href="index.php">Startseite</a></li>
+            <li><a href="Stammdatenverwaltung.php">Stammdatenverwaltung</a></li>
+            <li><a href="#">Neuanschaffungen</a></li>
+            <li><a href="#">Wartungsarbeiten</a></li>
+            <li><a href="#">IT-Ausstattung</a></li>
+            <li><a href="#">Abfragen</a></li>
+        </ul>
+    </div>
+    <!--/ Column width 3-->
+    <?php
 }
